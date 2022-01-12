e3cb7264dd15c73940b9057a0d231cf0dce11711
gndpwnd <gnelsondirect@outlook.com>
2022-01-11 21:33:54 -0900

    user_login ps

---
+++ ps/ad_user_login.ps1
@@ -0,0 +1,10 @@
+$usern = "administrator"
+$passw = "passwurd"
+$domainObj = [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+$PDC = ($domainObj.PdcRoleOwner).Name
+$SearchString = "LDAP://"
+$SearchString += $PDC + "/"
+$DistinguishedName = "DC=$($domainObj.Name.Replace('.', ',DC='))"
+$SearchString += $DistinguishedName
+New-Object System.DirectoryServices.DirectoryEntry($SearchString, $usern,
+$passw)
